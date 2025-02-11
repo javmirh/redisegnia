cf7d3a76777624e48f755be1ff1808403b92da96
javmirh <javmirdev@gmail.com>
2025-02-11 19:34:52 +0100

    database url changed

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './config/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url:'***REMOVED***neondb_owner:npg_uTA2mif7eIHK@ep-square-sky-a4pomlr9.us-east-1.aws.neon.tech/redisegna?sslmode=require',
+    url:process.env.DATABASE_URL,
   },
 });
